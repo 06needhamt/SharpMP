7038a9a8def5b4a0e74956ec4968973e10d34347
Tom Needham <06needhamt@gmail.com>
2017-11-12 14:56:07 +0000

    Add Simple TestApp

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharpMP;
+using System.Threading;
 
 namespace TestApp
 {
@@ -11,8 +13,24 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-            // Test();
+            Test();
             Console.ReadKey();
+        }
+
+        static void Test()
+        {
+            int defaultDevice = OpenMP.GetDefaultDevice();
+            Console.WriteLine("Default Defice ID: {0}", defaultDevice);
+
+            for (int i = 0; i < 10; i++)
+            {
+                double wTime = OpenMP.GetWTime();
+                Console.WriteLine("WTime: {0}", wTime);
+                Thread.Sleep(1000);
+            }
+
+            double wTick = OpenMP.GetWTick() * Math.Pow(10,7);
+            Console.WriteLine("WTick: {0}", wTick);
         }
     }
 }
